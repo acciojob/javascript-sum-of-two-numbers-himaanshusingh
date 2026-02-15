31f761525ce68717a5155102ea99a412dd1d40fc
Himanshu Singh <himanshusingh1k@gmail.com>
2026-02-15 10:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+let input1 = parseInt(prompt("Enter the first number:"));
+let input2 = parseInt(prompt("Enter the second number:"));
 
-// Write your code here and print the output using alert function
+function isValidNumber(value) {
+  return !isNaN(value) && value !== "";
+}
+
+if (isValidNumber(input1) && isValidNumber(input2)) {
+  let sum = input1 + input2;
+  alert(`The sum of ${input1} and ${input2} is ${sum}.`);
+} else alert("Invalid input. Please enter a valid number.");
